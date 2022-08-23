99fb9988f6ca804bd7b846f50db9b42ff3b19cac
Alexandre Corda <ChikiChikiB4mB4m>
2022-08-22 22:54:28 -0300

    - geração do footer - geração do header - bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gestao-pokemon';
+  route = '/';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        this.route = event.url;
+      }
+    });
+  }
 }
